252b0e83cc29afe31b0a11e3fe38bc948eabcc17
Subbotin <subbotinkom@gmail.com>
2020-10-16 11:55:06 +0300

    Lesson 5. Basket total price

--- mainapp/views.py
+++ mainapp/views.py
@@ -18,9 +18,11 @@ def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
 
-    basket = []
+    total_price = 0
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
+        for el in basket:
+            total_price += el.product.price * el.quantity
     if pk is not None:
         if pk == 0:
             products = Product.objects.all().order_by("price")
@@ -34,7 +36,7 @@ def products(request, pk=None):
             "category": category,
             "products": products,
             "media_url": settings.MEDIA_URL,
-            "basket": basket,
+            "total_price": total_price,
         }
         return render(request, "mainapp/products_list.html", content)
     similar_products = Product.objects.all()
@@ -43,7 +45,7 @@ def products(request, pk=None):
         "links_menu": links_menu,
         "similar_products": similar_products,
         "media_url": settings.MEDIA_URL,
-        "basket": basket,
+        "total_price": total_price,
     }
     return render(request, "mainapp/products.html", content)
 
